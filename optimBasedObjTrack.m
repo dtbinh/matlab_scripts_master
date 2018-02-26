0e37075b4021b015f29e236b42b6733d36b5a089
line <vegalin@stud.ntnu.no>
2018-02-26 12:04:50 +0100

    Adde landing pad simulation

--- optimBasedObjTrack.m
+++ optimBasedObjTrack.m
@@ -5,23 +5,9 @@ addpath('./functions/')
 N = 30;
 dt=1;
 
-%% Variables
-%x_u/l=[p_x, p_y, v_x, v_y]
-%x=[p_l-p_u]
-%x_l=zeros(4,N);
-
-% Inital conditions
-%x_l(1:2,1)=[30, 110];       %Pos
-%x_l(3:4,1)=[7, 7];          %Vel
-
+%% Inital conditions
 x0=[240; 150];
 u0=[12; -7.25];
-
-%% Simulate Landing pad with constant linear vel
-% for k=1:length(x_l)-1
-%     x_l(3:4,k+1)=x_l(3:4,k);
-%     x_l(1:2,k+1)=x_l(1:2,k)+x_l(3:4,k)*dt;
-% end
 
 %% Define the dynamics and control parameters
 A = eye(2);
@@ -59,6 +45,11 @@ v_c_max=10;          %Mav vel UAV in m/s
 a_max=2;             %Max acceleration for the uav given in m/s^2
 [x_uav,u_uav]=optimFunc(A,B,Q,R,x0,u0,x_l,x_u,u_l,u_u,du_l,du_u,N); %% Get init values
 
+% Variables used for simulating the landing pad
+q_l=zeros(4,length(ti));
+q_l(1:2,1)=[30, 110];       %Init pos
+q_l(3:4,1)=[7, 7];          %Init vel
+
 for i=1:length(ti)
     if mod(ti(i),dt)==0 %% Run the optimization
         [x_uav,u_uav]=optimFunc(A,B,Q,R,x0,u0,x_l,x_u,u_l,u_u,du_l,du_u,N,x_uav);
@@ -66,13 +57,23 @@ for i=1:length(ti)
         u0=u_uav(:,1);
     end
     
+    %Simulate quadcopter
     q_dot_u=quadcopter(q_u(:,i),u0,8);
     q_u(:,i+1)=q_u(:,i)+q_dot_u*dti;
     
-    p.setPose([0,0,0],[q_u(1:2,i);0;0]);
+    % Simulate Landing pad with constant linear vel (Pure integration)
+    q_l(3:4,i+1)=q_l(3:4,i);
+    q_l(1:2,i+1)=q_l(1:2,i)+q_l(3:4,i)*dti;
+    
+    %Plot simulation
+    p.setPose([q_l(1:2,i);pi/4],[q_u(1:2,i);0]);
     pause(dti/speed);
 end
 
+%% Plotting
+plot(ti,q_u(1:2,1:end-1))
+figure 
+plot(ti,q_u(3:4,1:end-1))
 
 %% Functions
 
